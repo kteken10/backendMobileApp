20b6c422c8e9542286b5c515bdda7dd57ab1bc19
Dissang Tenkam <dissangfrancis@yahoo.com>
2023-06-25 04:57:22 +0100

    Continue test

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-from flask import Flask
+import os
+from os import environ as env
+from urllib.parse import quote_plus, urlencode
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, redirect, session, jsonify, url_for, request
+# from flask_migrate import Migrate
+from flask_cors import CORS
+from models import db, Visiteur, Fournisseur, Automobile
+from config import config
+from datetime import datetime
 
 # Créer une instance de l'application Flask
+
 app = Flask(__name__)
+env_type = 'development'
+secret_key = os.urandom(24).hex()
+app.secret_key = secret_key
+app.config.from_object(config[env_type])
+db.init_app(app)
+# migrate = Migrate(app, db)
+
+# Création des tables
+with app.app_context():
+    db.create_all()
+
+CORS(app)  # Pour autoriser les requêtes CORS
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
 
 # Définir une route pour la page d'accueil
 @app.route('/')
