d2bed3fc357f79822174692840efb4e6925f3d1f
kteken10 <dissangfrancis@yahoo.com>
2023-07-07 06:22:52 +0200

    Beginning To insert the default Token into the test_api

--- test_api.py
+++ test_api.py
@@ -1,8 +1,16 @@
 #Fichier qui utilise pytest pour les test unitaire conernant l'API DE l'APP
 import pytest 
+import os
+from os import environ as env
 from flask_testing import TestCase
 from app import app
 
+from dotenv import find_dotenv, load_dotenv
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+    
+    
 class TestApi(TestCase):
     def create_app(self):
         return app
@@ -17,3 +25,21 @@ class TestApi(TestCase):
         response = self.client.post('/visiteurs', json=data)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json['message'], 'Visiteur created successfully')    
+    def test_get_visiteur(self):
+        visiteur_id = 1
+
+        response = self.client.get(f'/visiteurs/{visiteur_id}')
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json['message'], 'Token missing')
+
+        # Utiliser un token valide pour accéder à un visiteur spécifique
+        token = env.get("TOKEN_TEST", None)
+        headers = {'Authorization': token}
+        response = self.client.get(f'/visiteurs/{visiteur_id}', headers=headers)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json, dict)
+
+    def test_delete_all_visiteurs(self):
+        response = self.client.delete('/visiteurs')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json['message'], 'Tous les visiteurs ont été supprimés avec succès')    
